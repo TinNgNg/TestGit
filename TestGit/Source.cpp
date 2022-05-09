ecd531a125a3e064a79eebc5828832783f795bfb
Tín Nguyễn Ngọc <21521538@ms.uit.edu.vn>
2022-05-09 21:57:44 +0700

    Phai sua thay doi 1 cai gi do roi moi push duoc

--- TestGit/Source.cpp
+++ TestGit/Source.cpp
@@ -6,5 +6,7 @@ int main()
 	cout << "Test push git" << endl;
 	cout << "Chua ranh ve viec xai lam " << endl;
 	cout << "Thu pull mot cai gi do ve" << endl;
+	cout << "Them mot cai gi do cho co ve thay doi";
+	cout << "Them 2 dong thi no van la 1 thay doi";
 	return 1;
 }
