059380e58be159888e78bad7214cfd23a51137b2
tinanh525 <63497752+tinanh525@users.noreply.github.com>
2022-05-09 22:22:47 +0700

    Conflict

--- TestGit/Source.cpp
+++ TestGit/Source.cpp
@@ -8,5 +8,6 @@ int main()
 	cout << "Thu pull mot cai gi do ve" << endl;
 	cout << "Them mot cai gi do cho co ve thay doi";
 	cout << "Them 2 dong thi no van la 1 thay doi mot cai gi do";
+	cout << "Dong nay chi mang tinh chat pha";
 	return 1;
 }
