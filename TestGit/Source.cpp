f5a08ff23bd5135be89cdde253b1a47139cc907c
tinanh525 <63497752+tinanh525@users.noreply.github.com>
2022-05-09 22:19:19 +0700

    conflict choi cho vui

--- TestGit/Source.cpp
+++ TestGit/Source.cpp
@@ -7,6 +7,6 @@ int main()
 	cout << "Chua ranh ve viec xai lam " << endl;
 	cout << "Thu pull mot cai gi do ve" << endl;
 	cout << "Them mot cai gi do cho co ve thay doi";
-	cout << "Them 2 dong thi no van la 1 thay doi";
+	cout << "Them 2 dong thi no van la 1 thay doi 1 thu gi do";
 	return 1;
 }
