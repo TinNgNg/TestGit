e0c82459562c76f53a014c86884dd99276af9eee
AmosIvor <trantuanvu00000@gmail.com>
2022-07-15 10:50:27 +0700

    Vu change

--- TestGit/index.js
+++ TestGit/index.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 function Helo()
 {
     return 5;
 }
 Helo();
-=======
+
 // Hello, this if index.js file
 
 function hello() {
@@ -12,8 +11,6 @@ function hello() {
 }
 
 hello();
-<<<<<<< HEAD
->>>>>>> db586e1a0ba8c488a42c588ed4aa198f9eaba616
-=======
+
 //thu
->>>>>>> c8551ca9b0996d922e71ea7f1be3eb991d3a133a
+
